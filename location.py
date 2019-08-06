1c0be4975458a8983b7dafda082d460d1906ab8c
Kathy Ren <dondelus@gmail.com>
2019-08-05 20:40:50 -0400

    Playing around with getting locations

---
+++ location.py
@@ -0,0 +1,17 @@
+import googlemaps
+import uuid
+from datetime import datetime
+
+API_KEY = 'AIzaSyDc-I-c7IHqNzJhn8SQLtBlXsne3UF3lOM'
+GEO_URI = 'https://www.googleapis.com/geolocation/v1/geolocate?key='
+
+def main():
+	gm = googlemaps.Client(key=API_KEY)
+
+
+def get_user_location(gmaps):
+
+
+
+if __name__ == '__main__':
+	main()
